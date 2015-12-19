b3b408fa04d30c4cabc871a38b598f9702a7c4e2
Michael Lubensky <lubenskyy@gmail.com>
2015-12-19 15:56:06 -0800

    Creating IPerson Interface

---
+++ src/main/java/com/sqa/ml/employees/IPerson.java
@@ -0,0 +1,28 @@
+/**
+ * File Name: IPerson.java<br>
+ * Lubensky, Michael<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Dec 19, 2015
+ */
+package com.sqa.ml.employees;
+
+/**
+ * IPerson //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Lubensky, Michael
+ * @version 1.0.0
+ * @since 1.0
+ */
+public interface IPerson {
+
+	final static int MAX_AGE = 110;
+
+	public String getName();
+
+	public boolean move();
+}
