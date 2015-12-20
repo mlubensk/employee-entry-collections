0a953d8f505c33c5276c9dbdcfb20ba4dd3450cb
Michael Lubensky <lubenskyy@gmail.com>
2015-12-19 16:38:45 -0800

    Adding getters and setters

---
+++ src/main/java/com/sqa/ml/employees/AbstractPerson.java
@@ -0,0 +1,107 @@
+/**
+ * File Name: AbstractPerson.java<br>
+ * Lubensky, Michael<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Dec 19, 2015
+ */
+package com.sqa.ml.employees;
+
+/**
+ * AbstractPerson //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Lubensky, Michael
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class AbstractPerson implements IPerson {
+
+	private String address;
+
+	private int age;
+
+	private String name;
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return this.address;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return this.age;
+	}
+
+	/**
+	 * @return the name
+	 */
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * @see com.sqa.ml.employees.IPerson#move()
+	 */
+	@Override
+	public boolean move() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * @param address
+	 *            the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @param age
+	 *            the age to set
+	 */
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(int name) {
+		this.name = name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("AbstractPerson [address=");
+		builder.append(this.address);
+		builder.append(", age=");
+		builder.append(this.age);
+		builder.append(", name=");
+		builder.append(this.name);
+		builder.append("]");
+		return builder.toString();
+	}
+}
